d94acc187ca8abc5ef4f78937176dc7008b2bda2
Parth Dedaniya <parth.dedaniya@simformsolutions.com>
2023-02-06 17:48:59 +0530

    Create routes and separate files, styling image slider, functions for routes

--- script.js
+++ script.js
@@ -2,66 +2,56 @@ home = document.getElementById("home");
 about = document.getElementById("about");
 images = document.getElementById("images");
 services = document.getElementById("services");
-content = document.getElementsByClassName("content");
+content = document.getElementById("content");
 selected = document.getElementsByClassName("navbar");
-
-window.addEventListener("load", (e) => {            //When window load after refresh, it will show content based what URL is.
-    fetchContent(e.target.location.hash)
-})
-
-Array.from(selected).forEach(ele => {                       //When we click on buttons, it will show content based on URL.
-    ele.addEventListener("click", (e) => {
-        fetchContent(e.target.attributes.href.nodeValue);
-    })
-})
-
-function fetchContent(e) {
-    switch(e) {
-
-        case "#":
-            home.style.display = "block";
-            about.style.display = "block";
-            images.style.display = "none";
-            services.style.display = "none";
-            break;
-        
-        case "#about":
-            home.style.display = "none";
-            about.style.display = "block";
-            images.style.display = "none";
-            services.style.display = "none";
-            break;
-        
-        case "#images":
-            home.style.display = "none";
-            about.style.display = "none";
-            images.style.display = "block";
-            showSlide(slideindex);              //to display only one image at a time for first time
-            services.style.display = "none";
-            break;
-        
-        case "#services":
-            home.style.display = "none";
-            about.style.display = "none";
-            images.style.display = "none";
-            services.style.display = "block";
-            break;
-        
-        case "":
-            home.style.display = "block";
-            about.style.display = "block";
-            images.style.display = "none";
-            services.style.display = "none";
-            break;
-        
-        default:
-            break;
-    }
-}
-
 slider = document.getElementsByClassName("imgslide");
 slideindex = 0;
-slider[slideindex].style.display = "block";
+
+window.addEventListener("load", async (e) => {
+    const res = await fetch(routes[e.target.location.hash]);
+    const data = await res.text();
+    content.innerHTML = data;
+    if (e.target.location.hash == "#images") {
+        for (let i = 0; i < slider.length; i++) {
+            slider[i].style.display = "none";
+        }
+        slider[0].style.display = "block";
+    }
+})
+
+window.addEventListener("popstate", async (e) => {
+    const res = await fetch(routes[e.target.location.hash]);
+    const data = await res.text();
+    content.innerHTML = data;
+    if (e.target.location.hash == "#images") {
+        for (let i = 0; i < slider.length; i++) {
+            slider[i].style.display = "none";
+        }
+        slider[0].style.display = "block";
+    }
+})
+
+const routes = {
+    "": "./home.html",
+    "#": "./home.html",
+    "#about": "./about.html",
+    "#images": "./images.html",
+    "#services": "./services.html",
+}
+
+Array.from(selected).forEach(ele => {                       //When we click on buttons, it will show content based on URL.
+    ele.addEventListener("click", async (e) => {
+        const res = await fetch(routes[e.target.attributes.href.nodeValue]);
+        const data = await res.text();
+        content.innerHTML = data;
+        if (e.target.attributes.href.nodeValue == "#images") {
+            for (let i = 0; i < slider.length; i++) {
+                slider[i].style.display = "none";
+            }
+            slider[0].style.display = "block";
+        }
+    })
+})
 
 function showSlide(slideindex) {
     for (let i = 0; i < slider.length; i++) {
@@ -76,4 +66,7 @@ function changeslide(num) {
     }
     slideindex = (slideindex + num) % slider.length;  //Here direct used num as -1 for left and +1 for right.
     showSlide(slideindex);
+}
+function scrollFun() {
+    document.querySelector("#about").scrollIntoView(true,{behaviour: "smooth"});
 }
